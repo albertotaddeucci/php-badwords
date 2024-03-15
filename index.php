1f758441a1b72ea352d60d80f4f679b123ce3a4d
alberto <alberto.taddeucci@gmail.com>
2024-03-15 15:51:18 +0100

    aggiunta stile

--- index.php
+++ index.php
@@ -8,26 +8,32 @@
 
     <!-- bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <!-- my style -->
+    <link href="style.css" rel="stylesheet">
+
+
 </head>
 
 <body>
 
 
-    <div class="container">
+    <div class="container mt-3">
+        <h1 class="my_accent text-center">Controllo Badwords</h1>
 
-        <form action="results.php">
+        <form action="results.php" class="mt-5">
 
             <div class="mb-3">
-                <label for="text" class="form-label">Testo completo</label>
+                <label for="text" class="form-label fw-bold ">Testo completo</label>
                 <textarea class="form-control" id="text" name="text" rows="3"></textarea>
             </div>
 
             <div class="mb-3">
-                <label for="badwords" class="form-label">Parola da censurare</label>
+                <label for="badwords" class="form-label fw-bold ">Parola da censurare</label>
                 <input type="text" class="form-control" name="badword" id="badwords">
             </div>
 
-            <button type="submit">Ottieni risultato</button>
+            <button type="submit" class="btn btn-danger mt-3">Ottieni risultato</button>
 
 
         </form>
--- results.php
+++ results.php
@@ -15,30 +15,59 @@ $text_replace = str_replace($input_badword, "***", $input_text);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Risultati</title>
+
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+
+    <!-- my style -->
+    <link href="style.css" rel="stylesheet">
 </head>
 
 <body>
 
-    <h1>Risultati</h1>
+    <div class="container my-4">
+        <h1 class="my_accent text-center">Risultati</h1>
 
-    <h2>Testo originale</h2>
 
-    <p>
-        <?php echo $input_text ?>
+        <div class="row gap-2 mt-4">
+            <div class="col-auto my_sec_accent flex-grow-1 border border-1 border-black  rounded-2">
 
-    </p>
+                <h2 class="mt-3">Testo originale</h2>
 
-    <p> La lunghezza del testo è pari a <?php echo strlen($input_text) ?> caratteri </p>
+                <p class="p-2 fst-italic">
+                    <?php echo $input_text ?>
 
-    <h2>Testo censurato</h2>
+                </p>
 
-    <p>
-        <?php echo $text_replace ?>
+                <p class="border border-2 border-black fw-bold text-danger p-2 bg-light rounded-3 text-center"> La lunghezza del testo è pari a <?php echo strlen($input_text) ?> caratteri </p>
 
-    </p>
 
-    <p> Adesso la lunghezza è di <?php echo strlen($text_replace) ?> caratteri</p>
+            </div>
+            <div class="col-auto my_sec_accent flex-grow-1 border border-1 border-black rounded-2">
 
+                <h2 class="mt-3">Testo censurato</h2>
+
+                <p class="p-2 fst-italic">
+                    <?php echo $text_replace ?>
+
+                </p>
+
+                <p class="border border-2 border-black fw-bold text-danger p-2 bg-light rounded-3 text-center "> Adesso la lunghezza è di <?php echo strlen($text_replace) ?> caratteri</p>
+
+            </div>
+        </div>
+
+
+
+
+
+    </div>
+
+
+
+    <!-- bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
 </body>
 
